255b3e2a0d2445494f703461caf4ee7c381bb8b4
vuhk <qwerty22121998@gmail.com>
2024-05-22 22:04:54 +0700

    rem unused dto

--- pkg/dto/node.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type Shape string
-
-type Node struct {
-	Shape Shape  `json:"shape"`
-	ID    string `json:"id"`
-	Text  string `json:"text"`
-}
--- pkg/dto/subgraph.go
+++
@@ -1,5 +0,0 @@
-package dto
-
-type SubGraph struct {
-	Nodes []*Node `json:"nodes"`
-}
